8c059152bb3c7bcfb9560b1b90b70fda9072d30b
abhinav <abhinavsuresh159@gmail.com>
2024-05-13 17:25:45 +0530

    13/05/2024

--- chemfinance/placecode/serializers/country.py
+++ chemfinance/placecode/serializers/country.py
@@ -6,3 +6,10 @@ class CountrySerializer(serializers.ModelSerializer):
     class Meta:
         model = Country
         fields = "__all__"
+
+    def validate(self, attrs):
+        country_code = attrs['country_code']
+        if Country.objects.filter(country_code=country_code).exists():
+            raise serializers.ValidationError({"country_code": "A country with this country code already exists"})
+        return attrs
+        
